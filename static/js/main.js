cbc0d631434e3f6e0dba546a4e8aa4b5ede7ba7b
Henry-2016 <jacob2016henry@gmail.com>
2021-03-01 06:35:03 +0300

    New Bake4me client Staff Admin Server Uploads

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,18 @@
 
+var data_src_url = "http://127.0.0.1:6060/";
+// var data_src_url = "http://192.168.43.140:6060/";
+
 let url = 'http://localhost/';
-let html_pages_source_dir = 'bake4me-repos/';
+let html_pages_source_dir = 'bake4me-client-staff-admin-server-repo/';
 let php_pages_source_dir = 'bake4me-php/';
 
 
 // BAKERY NAVIGATION ...........
-function Load_Client_Register_Page () {window.location = 'bakery_register.html';}
 function Load_Cakes_Page () {window.location='bakery_cakes.html'}
-function Load_Cakes_Page () {window.location='bakery_cakes.html'}
+function Bakery_Register () {window.location='bakery_register.html'}
+function Bakery_Log_Out () {window.location='clientindex.html'}
+function Bakery_Log_In () {window.location='clientindex.html'}
+
 
 // STAFFS
 function Load_Crud_Page () {window.location='staff_bakery_crud.html'}
@@ -17,13 +22,15 @@ function Load_Crud_Page () {window.location='staff_bakery_crud.html'}
 function Load_Outlets_Page () {window.location='bakery_outlets.html';}
 
 // SCHOOL NAVIGATION ..... 
-function Load_School_Index_Page () {window.location = 'school_index.html';}
-function Load_School_Login_Register_Page () {window.location = 'school_login_register.html';}
-function School_Logout () {window.location = url+php_pages_source_dir+'school_logout.php';}
-
+// function Load_School_Index_Page () {window.location = 'school_index.html';}
+function  Load_School_Login_Register () {window.location = 'school_login_register.html';}
+// function School_Logout () {window.location = url+php_pages_source_dir+'school_logout.php';}
+function School_LogOut () {window.location='school_login_register.html';}
+function School_LogIn () {window.location='school_login_register.html';}
 function Load_School_Notice_Board () {window.location = 'school_notice_board.html';}
-// function School_LogIn_Only () {window.location = url+html_pages_source_dir+'school_login.html';}
-function Load_School_SubLink_Payment () {window.location = url+html_pages_source_dir+'school_payment.html';}
+function Load_School_Materials () {window.location = 'school_materials.html';}
+function Load_School_Calender () {window.location = 'school_calender.html';}
+function Load_School_Payment () {window.location = 'school_payment.html';}
 
 
 // BAKING MACHINES .....
@@ -44,4 +51,17 @@ function Display_Register_Form ()
 {
     document.getElementById("school-login-div-id").style.display="none";
     document.getElementById("school-register-div-id").style.display="block";
+}
+
+
+
+// Menu Gallery
+function Add_Gallery_Iframe (endpoint)
+{
+    // document.getElementById("offline-label-div").style.display = "none";
+    let MenuGalleryDiv = document.getElementById("menu-pics-div");
+    let MenuIframe = document.createElement("iframe");
+    MenuIframe.className = "menu-iframe";
+    MenuIframe.setAttribute("src",  data_src_url + endpoint);
+    MenuGalleryDiv.appendChild(MenuIframe); 
 }
--- static/js/staff.js
+++ static/js/staff.js
@@ -5,6 +5,9 @@ function Load_Customer_Orders_Page () {window.location='staff_bakery_orders.html
 function Load_Customer_Deliveries_Page () {window.location='staff_bakery_delivery.html'}
 function Load_Customer_Feedbacks_Page () {window.location='staff_bakery_feedbacks.html'}
 function Load_Customer_History_Page () {window.location='staff_bakery_history.html'}
+function Staff_Bakery_LogOut () {window.location='staffindex.html'}
+function Staff_Bakery_LogIn () {window.location='staffindex.html'}
+
 
 //  SCHOOL NAVIGATION 
 function Load_Students_NoticeBoard_Page () {window.location='staff_school_noticeboard.html'}
@@ -13,6 +16,8 @@ function Load_Students_New_Page () {window.location='staff_school_new.html'}
 function Load_Students_New_Approve_Page () {window.location='staff_school_new_approve.html'}
 function Load_Students_Payments_Page () {window.location='staff_school_payments.html'}
 function Load_Students_Registered_Page () {window.location='staff_school_registered.html'}
+function Staff_School_LogOut () {window.location='staffindex.html'}
+function Staff_School_LogIn () {window.location='staffindex.html'}
 
 
 
